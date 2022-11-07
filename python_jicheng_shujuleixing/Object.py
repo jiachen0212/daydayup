c37519d2931422f7a8514ee5ecc0f74b9d1e7850
陈佳 <chenjia@chenjiadeMacBook-Pro.local>
2022-11-07 15:19:13 +0800

    learn face Object

--- python_jicheng_shujuleixing/Object.py
+++ python_jicheng_shujuleixing/Object.py
@@ -1,10 +1,27 @@
 # coding=utf-8
 # 面向对象编程
 
-# 1. __name 前置双下划线, 仅可内部访问, 外部不可
-# 2. __fun__ 前后均双下划线, 是类的内置方法. 
+# 1. 
+# __name 前置双下划线, 仅可内部访问, 外部不可
+# __fun__ 前后均双下划线, 是类的内置方法. 
+class Student:
 
-# __call__
+    def __init__(self, name, score):
+        self.name = name
+        self.__score = score
+
+    # 定义打印学生信息的方法
+    def show(self):
+        # __score可在类内访问, 但直接student1.__score会出错..
+        print("Name: {}. Score: {}".format(self.name, self.__score))
+
+# 实例化，创建对象
+student1 = Student("jia.chen", 100)
+student1.show()  # 打印 Name: John, Score: 100
+# student1.__score  # 打印出错，该属性不能从外部访问. 
+
+
+# 2. __call__
 class Person(object):
 
     def __init__(self, name, age):
@@ -19,7 +36,7 @@ print(p())   # 因为类中有__call__, 故可这样调用类
 
 
 
-# __str__
+# 3. __str__
 class Person(object):
 
     def __init__(self, name, age):
@@ -34,7 +51,7 @@ print(str(p))
 print(p)    # 等价
 
 
-# __repr__
+# 4. __repr__
 class Person(object):
 
     def __init__(self, name, age):
@@ -54,7 +71,7 @@ print(repr(p))  # Person('baozi', 26)
 
 
 
-# __getitem__()、__setitem__()、__delitem__() 类似字典的取值, 赋值, 删除 功能
+# 5. __getitem__()、__setitem__()、__delitem__() 类似字典的取值, 赋值, 删除 功能
 class Person(object):
 
     def __init__(self, name, age):
@@ -70,7 +87,7 @@ p = Person("baozi", 26)
 print(p["age"])    # 通过 [] 取值时，调用了我
 
 
-# @staticmethod: 装饰为静态方法, 就不需要传入self参数也可正常访问. 
+# 6. @staticmethod: 装饰为静态方法, 就不需要传入self参数也可正常访问. 
 class Person(object):
     def __init__(self, name, age):
         self._name = name
@@ -89,7 +106,7 @@ print(p.static_talk('chenjia', 26))
 
 
 
-# @classmethod: 装饰为类方法, 无需实例化, 就可访问.
+# 7. @classmethod: 装饰为类方法, 无需实例化, 就可访问.
 class Person(object):
     def __init__(self, name, age):
         self._name = name
@@ -102,6 +119,56 @@ class Person(object):
 print(Person.static_talk("jia.chen", 26))
 
 
+
+# 8. 类的继承: 继承父类初始化, 子类中重写一些方法. 
+# 创建父类学校成员SchoolMember
+class SchoolMember:
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def tell(self):
+        # 打印个人信息
+        print('Name:"{}" Age:"{}"'.format(self.name, self.age), end=" ")
+
+
+# 创建子类老师 Teacher
+class Teacher(SchoolMember):
+
+    def __init__(self, name, age, salary):
+        # 利用父类进行初始化
+        SchoolMember.__init__(self, name, age) 
+        # 新添加薪水 属性 
+        self.salary = salary
+
+    # 重写tell方法, 报告薪水.
+    def tell(self):
+        SchoolMember.tell(self)
+        print('Salary: {}'.format(self.salary))
+
+
+# 创建子类学生Student
+class Student(SchoolMember):
+
+    def __init__(self, name, age, score):
+    # 利用父类进行初始化
+        SchoolMember.__init__(self, name, age)
+        self.score = score
+    # 重写tell方法, 报告分数.
+    def tell(self):
+        SchoolMember.tell(self)  # 等价于, super().tell() 
+        super().tell()  # super调用父类的方法. 等同于SchoolMember.tell(self)
+        print('score: {}'.format(self.score))
+
+
+teacher1 = Teacher("John", 44, "$60000")
+student1 = Student("Mary", 12, 99)
+teacher1.tell()  # 打印 Name:"John" Age:"44" Salary: $60000
+student1.tell()  # Name:"Mary" Age:"12" score: 99
+
+
+# 9. 
 
 
 
